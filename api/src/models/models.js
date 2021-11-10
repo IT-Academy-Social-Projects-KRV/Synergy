25a8662b8dbe39b81f97400c9e7d2a6c8699946a
DimasCoder <dimakulykovych@gmail.com>
2021-11-10 16:44:01 +0200

    Setup connection to db, created model Project in models.js

---
+++ api/src/models/models.js
@@ -0,0 +1,7 @@
+const sequelize = require('../db')
+const {DataTypes} = require('sequelize')
+
+const Project = sequelize.define("project", {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING, allowNull: false}
+})
