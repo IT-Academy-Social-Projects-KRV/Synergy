807df39fc2277993af3aed738b5decf6796fcdac
DimasCoder <dimakulykovych@gmail.com>
2021-11-11 23:35:05 +0200

    Changed mock data

--- api/src/seeders/ProjectData.js
+++ api/src/seeders/ProjectData.js
@@ -2,17 +2,57 @@ const {models, Project} = require('../models1/models')
 
 const projects = [
     {
-        name: 'Create house',
+        name: 'Build house',
         createdAt: Date.now(),
         updatedAt: Date.now()
     },
     {
-        name: 'Create house1',
+        name: 'Computer Software Development',
         createdAt: Date.now(),
         updatedAt: Date.now()
     },
     {
-        name: 'Create house2',
+        name: 'Design of Plans',
+        createdAt: Date.now(),
+        updatedAt: Date.now()
+    },
+    {
+        name: 'New Product Development',
+        createdAt: Date.now(),
+        updatedAt: Date.now()
+    },
+    {
+        name: 'Maintenance of Process Industries',
+        createdAt: Date.now(),
+        updatedAt: Date.now()
+    },
+    {
+        name: 'Event or Relocations',
+        createdAt: Date.now(),
+        updatedAt: Date.now()
+    },
+    {
+        name: 'Equipment or System Installation',
+        createdAt: Date.now(),
+        updatedAt: Date.now()
+    },
+    {
+        name: 'Web app development',
+        createdAt: Date.now(),
+        updatedAt: Date.now()
+    },
+    {
+        name: 'Synergy CRM',
+        createdAt: Date.now(),
+        updatedAt: Date.now()
+    },
+    {
+        name: 'The International',
+        createdAt: Date.now(),
+        updatedAt: Date.now()
+    },
+    {
+        name: 'Coup de Grace',
         createdAt: Date.now(),
         updatedAt: Date.now()
     },
