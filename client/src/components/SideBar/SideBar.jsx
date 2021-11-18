4623e43bd2d3d01f7070b1ec1521d35d5680a0c4
AntonyJedi <barsafan96@gmail.com>
2021-11-18 21:06:57 +0200

    Added sidebar

--- client/src/components/SideBar/SideBar.jsx
+++ client/src/components/SideBar/SideBar.jsx
@@ -11,7 +11,7 @@ const SideBar = props => {
         <aside className={styles.sidebar + " " + props.sideClass}>
             <h4>Management</h4>
             <ul className={styles.sidebar_list}>
-                { role === 'admin' ? sidebarItems.Admin.map(item => <SideBarItem route={item.route} image={item.image} alt={item.alt} title={item.title} />) : ''}
+                { role === 'admin' ? sidebarItems.Admin.map(item => <SideBarItem key={item.title} route={item.route} image={item.image} alt={item.alt} title={item.title} />) : ''}
                 { role === 'customer' ? sidebarItems.Customer.map(item => <SideBarItem route={item.route} image={item.image} alt={item.alt} title={item.title} />) : ''}
             </ul>
         </aside>
