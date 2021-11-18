160af7cc221c82a2ce16a16653a97766f0e95890
AntonyJedi <barsafan96@gmail.com>
2021-11-19 00:11:06 +0200

    added_project_list

--- client/src/components/ProjectList/ProjectList.jsx
+++ client/src/components/ProjectList/ProjectList.jsx
@@ -1,9 +1,39 @@
+import React, {useEffect, useState} from 'react'
+import axios from "axios";
+import styles from './ProjectList.module.scss';
 import EmptyProjectList from "./EmptyProjectList"
+import ProjectListItem from "./ProjectListItem/ProjectListItem";
 
-const ProjectList =()=>{
+const ProjectList = () => {
+    let [data, setData] = useState([])
+    useEffect(() => {
+        axios.get('http://localhost:4000/api/project')
+            .then(response => {
+                console.log(response.data)
+                setData(response.data)
+            });
+    }, [])
     return(
         <div>
-            ProjectList
+            <h1 className={styles.title}>Projects List</h1>
+            <ul className={styles.list}>
+                <li className={styles.list_head}><div>ID</div>
+                    <div>Capital</div>
+                    <div>Added date</div>
+                    <div>Release date</div>
+                    <div>Description</div>
+                    <div>Owner</div>
+                </li>
+                {data.map(d => <ProjectListItem
+                    key={d.id}
+                    id={d.id}
+                    name={d.name}
+                    desc={d.description}
+                    capital={d.capital}
+                    start={d.date_start}
+                    finish={d.date_finish}
+                />)}
+            </ul>
         </div>
     )
 }
