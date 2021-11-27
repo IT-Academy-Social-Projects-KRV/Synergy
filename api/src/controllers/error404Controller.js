bb4299b719f86555fb316618b7a1d5c8b3dd84ba
Maria Savchuk <savchuk_maria@ukr.net>
2021-11-27 11:12:48 +0200

    Added router and controller for 404 page

---
+++ api/src/controllers/error404Controller.js
@@ -0,0 +1,9 @@
+const status = require('http-status');
+
+const get404ErrorPage = async (req, res, next) => {
+    res.status(status.NOT_FOUND).send('Page not found');
+}
+
+module.exports = {
+    get404ErrorPage,
+}
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,8 +3,10 @@ const Router = require('express');
 const router = new Router();
 const project = require('./projectRouter');
 const item = require('./itemRouter');
+const error404 = require('./error404Router');
 
 router.use('/project', project);
 router.use('/item', item);
+router.use('/*', error404);
 
 module.exports = router;
