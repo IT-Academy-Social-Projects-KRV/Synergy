4fc66dca5e66b187efa3a4bcc500c6969b477a1f
AntonyJedi <barsafan96@gmail.com>
2021-11-28 00:54:37 +0200

    Added post request.Added loader and alert

--- client/src/components/ProjectList/ProjectList.jsx
+++ client/src/components/ProjectList/ProjectList.jsx
@@ -1,56 +1,52 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, {useEffect, useState} from 'react';
 import styles from './ProjectList.module.scss';
 import EmptyProjectList from './EmptyProjectList';
 import ProjectListItem from './ProjectListItem/ProjectListItem';
-// import projectList from '../../consts/projectList';
+import {useHttp} from '../../hooks/http_hook';
+import Loader from '../Loader/Loader';
 
 const ProjectList = () => {
   const [data, setData] = useState([]);
+  const {get_request, loading} = useHttp();
   useEffect(() => {
-    axios
-      .get('http://localhost:4000/api/project')
-      .then((response) => {
-        setData(response.data);
-      })
-      .catch((err) => {
-        // setData(projectList);
-        throw err;
-      });
-
-    return () => setData([]);
+    (async () => {
+      const res = await get_request('project');
+      setData(res.data);
+    })();
   }, []);
 
   return (
-    <div>
-      {data.length > 0 ? (
-        <>
-          <h1 className={styles.title}>Projects List</h1>
-          <ul className={styles.list}>
-            <li className={styles.list_head}>
-              <div>ID</div>
-              <div>Capital</div>
-              <div>Added date</div>
-              <div>Release date</div>
-              <div>Description</div>
-              <div>Owner</div>
-            </li>
-            {data.map((d) => (
-              <ProjectListItem
-                key={d.id}
-                id={d.id}
-                name={d.name}
-                desc={d.description}
-                capital={d.capital}
-                start={d.date_start}
-                finish={d.date_finish}
-              />
-            ))}
-          </ul>
-        </>
-      ) : (
-        <EmptyProjectList />
-      )}
+    <div style={{height: '100%'}}>
+      {loading ? <Loader/> :
+        data.length > 0 ?
+          (
+            <>
+              <h1 className={styles.title}>Projects List</h1>
+              <ul className={styles.list}>
+                <li className={styles.list_head}>
+                  <div>ID</div>
+                  <div>Capital</div>
+                  <div>Added date</div>
+                  <div>Release date</div>
+                  <div>Description</div>
+                  <div>Owner</div>
+                </li>
+                {data.map((d) => (
+                  <ProjectListItem
+                    key={d.id}
+                    id={d.id}
+                    name={d.name}
+                    desc={d.description}
+                    capital={d.capital}
+                    start={d.date_start}
+                    finish={d.date_finish}
+                  />
+                ))}
+              </ul>
+            </>
+          ) : (
+            <EmptyProjectList/>
+          )}
     </div>
   );
 };
