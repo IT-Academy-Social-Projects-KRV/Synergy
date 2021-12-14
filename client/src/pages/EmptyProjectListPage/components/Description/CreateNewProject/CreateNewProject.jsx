2d1318c83916a24218a3e5708a48e25013f322e6
Matthew Mashtalyar <matviimashtalyar@gmail.com>
2021-12-14 07:15:12 +0200

    fixed link in empty projects component

--- client/src/pages/EmptyProjectListPage/components/Description/CreateNewProject/CreateNewProject.jsx
+++ client/src/pages/EmptyProjectListPage/components/Description/CreateNewProject/CreateNewProject.jsx
@@ -1,10 +1,18 @@
 import { Link } from '@mui/material';
+import { useHistory } from 'react-router-dom';
 import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 import styles from './styles.js';
+import routes from '../../../../../configs/routes.js';
 
 const CreateNewProject = () => {
+  const history = useHistory();
+
+  const handleClick = () => {
+    history.push(routes.pathToCreatingProject);
+  };
+
   return (
-    <Link sx={styles.link} href='#' underline='always'>
+    <Link sx={styles.link} underline='always' onClick={handleClick}>
       Do you want create new?
       <OpenInNewIcon fontSize='small' />
     </Link>
