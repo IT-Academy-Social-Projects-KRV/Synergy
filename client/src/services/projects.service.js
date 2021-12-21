69473caf31f1000706cc94894aadf0382f251816
Matthew Mashtalyar <matviimashtalyar@gmail.com>
2021-12-21 09:33:04 +0200

    fix formatting of code

--- client/src/services/projects.service.js
+++ client/src/services/projects.service.js
@@ -6,9 +6,28 @@ const getAllProjects = () => {
 
 // eslint-disable-next-line max-len
 const getSortProjects = (
-  { name, capital, dateStart, dateFinish, sortBy = 'capital', sortDirection = 'ASC' }, page = 1) => {
-  // eslint-disable-next-line max-len
-  return api.get(`/projects?size=10&sortBy=${sortBy}&sortDirection=${sortDirection}&capital=${capital[0]}-${capital[1]}&name=${name}&page=${page}&dateStart=${dateStart}&dateFinish=${dateFinish}`);
+  { 
+    name, 
+    capital, 
+    dateStart, 
+    dateFinish, 
+    sortBy = 'capital', 
+    sortDirection = 'ASC' 
+  }, 
+  page = 1
+) => {
+  return api.get(
+    `/projects?
+      size=10&
+      sortBy=${sortBy}&
+      sortDirection=${sortDirection}&
+      capital=${capital[0]}-${capital[1]}&
+      name=${name}&
+      page=${page}&
+      dateStart=${dateStart}&
+      dateFinish=${dateFinish}`
+  );
+};
 
 const getProjectById = (id) => {
   return api.get(`/projects/${id}`);
