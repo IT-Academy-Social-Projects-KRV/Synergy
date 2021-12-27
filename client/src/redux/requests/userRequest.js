49e1b6d7d13607786a40164bc6a3c820c15e4433
1Artemida <58728864+1Artemida@users.noreply.github.com>
2021-12-27 22:07:39 +0200

    linter fix

--- client/src/redux/requests/userRequest.js
+++ client/src/redux/requests/userRequest.js
@@ -1,24 +1,25 @@
 import { setIsLoader, setRequestError, setUser } from '..';
-// import statusCode from '../../consts/statusCode';
-// import { logIn } from '../../services/user.service';
+import statusCode from '../../consts/statusCode';
+import { logIn, userChanges } from '../../services/user.service';
+import { setIsAuthed } from '..';
 
 
 export const fetchLoginUser = (
-  //payload
+  payload
 ) => async (dispatch) => {
   dispatch(setIsLoader(true));
   try {
     sessionStorage.setItem('token', ('dfdsfsfsfsf'));
-    sessionStorage.setItem('user', JSON.stringify({ id: 1, roleId: 2 }));
+    sessionStorage.setItem('user', JSON.stringify({ id: 1, roleId: 2  }));
     dispatch(setUser(JSON.parse(sessionStorage.getItem('user'))));
-    // const res = await logIn(payload);
+    const res = await logIn(payload);
   
-    // if (res.status === statusCode.OK) {
-    //   sessionStorage.setItem('user',JSON.stringify(res.data.user));
-    //   sessionStorage.setItem('token', res.data.token);
-    //   dispatch(setUser(res.data.user));
-    //   dispatch(setIsAuthed(true));
-    // }
+    if (res.status === statusCode.OK) {
+      sessionStorage.setItem('user',JSON.stringify(res.data.user));
+      sessionStorage.setItem('token', res.data.token);
+      dispatch(setUser(res.data.user));
+      dispatch(setIsAuthed(true));
+    }
   }
   catch (e) {
     dispatch(setRequestError(e));
@@ -26,28 +27,44 @@ export const fetchLoginUser = (
   finally {
     dispatch(setIsLoader(false));
   }
-
-  // TODO: Create function for fetching user email in registration.
-  // export const fetchUserEmail = (payload) => (dispatch) => {
-  //   const { postRequest } = useHttp();
-
-  //   const fetchData = async () => {
-  //     try {
-  //       dispatch(setIsLoader(true));
-  //       const res = await postRequest('login/email', payload);
-  //       dispatch(setIsExistUser(res.data));
-  //     }
-  //     catch (e) {
-  //       dispatch(setRequestError(e));
-  //     }
-  //     finally {
-  //       dispatch(setIsLoader(false));
-  //     }
-  //   };
-  //   fetchData();
-  // };
 };
 
+export const fetchUpdateUser = (payload) => async (dispatch) => {
+  dispatch(setIsLoader(true));
+  try {
+    const res = await userChanges(payload);
+    if (res.status === statusCode.OK) {
+      sessionStorage.setItem('user',JSON.stringify(res.data[1][0]));
+      dispatch(setUser(res.data[1][0]));
+      return res.data[1][0];
+      
+    }
+  } catch (e) {
+    dispatch(setRequestError(e));
+  } finally {
+    dispatch(setIsLoader(false));
+  }
+};
+
+// TODO: Create function for fetching user email in registration.
+// export const fetchUserEmail = (payload) => (dispatch) => {
+//   const { postRequest } = useHttp();
+
+//   const fetchData = async () => {
+//     try {
+//       dispatch(setIsLoader(true));
+//       const res = await postRequest('login/email', payload);
+//       dispatch(setIsExistUser(res.data));
+//     }
+//     catch (e) {
+//       dispatch(setRequestError(e));
+//     }
+//     finally {
+//       dispatch(setIsLoader(false));
+//     }
+//   };
+//   fetchData();
+// };
 
 
 
