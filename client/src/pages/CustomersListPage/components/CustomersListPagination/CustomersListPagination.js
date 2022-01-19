698c3e5652de9f6d38af0496ca9c5baec3cd2d61
1Artemida <58728864+1Artemida@users.noreply.github.com>
2022-01-19 15:43:15 +0200

    create customers page with validation and search

---
+++ client/src/pages/CustomersListPage/components/CustomersListPagination/CustomersListPagination.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Pagination } from '@mui/material';
+
+const CustomersPagination = ({ currentPage, totalPages, setPaginateOptions }) => {
+  
+  return (
+    <Pagination
+      page={Number(currentPage)}
+      count={totalPages} 
+      onChange={setPaginateOptions}
+      shape='rounded'
+    />
+  );
+
+};
+
+export default CustomersPagination;
